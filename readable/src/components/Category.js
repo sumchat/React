fc47befaaf43dcd15f3d6eaaaa120fef8048340b
Suman Chatterjee <sum_chat@hotmail.com>
2017-09-02 10:26:02 -0400

    Add files via upload

---
+++ readable/src/components/Category.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { itemsFetchPosts } from '../actions';
+import { Link,withRouter } from 'react-router-dom';
+import {  Button, ButtonToolbar } from 'react-bootstrap'
+import Post_Comments_Toolbar from './Post_Comments_Toolbar'
+
+class Category extends Component {
+
+convertToUpper(name){
+  return name.toUpperCase();
+}
+componentDidMount(){
+  if (!this.props.posts)
+  {
+  this.props.fetchPosts();
+  }
+
+
+}
+
+getTime = timestamp =>{
+  let d = new Date(timestamp);
+  return d.toDateString();
+}
+  render() {
+    const category = this.props.categoryObj;
+    const {posts,postcols, type} = this.props;
+   let filteredposts = null;
+     if(posts)
+        filteredposts = posts.filter((post)=> post.deleted === false);
+
+return(
+  <tbody>
+        { type === "multiple" &&  <tr><td><div>  <Link to={{pathname:`/${category}`}}><h4>{this.convertToUpper(category)}</h4></Link></div></td></tr>}
+          {
+          filteredposts && filteredposts.map((post) =>{
+            if (post.category === category){
+            return(
+              <tr key={post.id}><td>
+              <Post_Comments_Toolbar type='post' id={post.id} from='category'/>
+
+              </td>{
+               postcols && postcols.map((col,i) => {
+                  if (col === "id"){
+                    return(
+                    <td key={i}>
+                    <Link to={{pathname:`/${category}/${post[col]}`}}>  {String(post[col])} </Link>
+                      </td>
+                    )
+                  }
+                  else if (col === "timestamp"){
+                    return (<td key={i}>
+                      {this.getTime(post[col])}
+                      </td>)
+                  }
+                  else{
+                    return (
+                  <td key={i}>
+                    {String(post[col])}
+                    </td>
+
+                  )
+                  }
+
+                })
+              }
+              </tr>
+
+            )}
+          })
+
+          }
+          </tbody>
+        )
+
+        }
+
+    }
+
+
+  const mapStateToProps = (state) => {
+      //console.log(state.Post.posts);
+      return {
+          posts: state.Post.posts,
+          postcols:state.Settings.postCols,
+      };
+  };
+  const mapDispatchToProps = (dispatch) => {
+      return {
+          fetchPosts: () => dispatch(itemsFetchPosts()),
+              }
+  }
+
+  export default withRouter(connect(mapStateToProps,mapDispatchToProps)(Category));
